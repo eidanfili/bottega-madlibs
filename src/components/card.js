25b5ca9907c6a326141f6e848cb8e3fee556b4a2
eidanfili <eidan@ledezma.org>
2018-10-03 09:34:55 -0600

    two way binding with inputs

--- src/components/card.js
+++ src/components/card.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
- import Input from './input';
- class Card extends Component {
+
+import Input from './input';
+
+class Card extends Component {
 
     constructor() {
         super()
 
         this.state = {
-            color: 'Blue',
+            color: 'BLUE',
             pluralNoun: ''
         }
 
-        this.handleInputChange();
-    }
-    
-    handleInputChange() {
-        this.setState({ color: 'red'})
-    }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    } 
 
+    handleInputChange(event) {
+        this.setState({ color: event.target.value })
+    }
 
     render() {
 
         return (
             <div className="card">
                 <h1>{this.state.color}</h1>
-                <input/>
+                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
             </div>
-            
         )
 
         return (
@@ -37,4 +37,5 @@ import React, { Component } from 'react';
         )
     }
 }
- export default Card; 
+
+export default Card;
--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        { Header("Bottega MadLibs", "cool content lol bro") }
+        { Header("Bottega MadLibs", "cool content bro") }
         <Card />
       </div>
     );
